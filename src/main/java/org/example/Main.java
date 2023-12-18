b6a547a9007f352b89dafc3267baa93728a72332
sallesalex01 <alex.salles2007@gmail.com>
2023-12-18 11:32:22 -0300

    Change server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,22 +7,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> listaDeCodigos = new ArrayList<>();
         Gson gson = new Gson();
-        listaDeCodigos.add("123456");
+        listaDeCodigos.add("1234567890");
+
+
+        JFrame frame = new JFrame("Controle de Acesso servidor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLayout(new FlowLayout());
+        JLabel codigoLabel = new JLabel("Codigo de acesso: ");
+        JTextField codigoTextField = new JTextField(20);
 
         port(3000);
-
         post("/api/verificarCodigo", (request, response) -> {
             String corpoRequest = request.body();
             System.out.println("Corpo JSON: " + corpoRequest);
             CodigoDeAcesso codigo = gson.fromJson(corpoRequest, CodigoDeAcesso.class);
+            codigoTextField.setText(codigo.codigo);
 
             if (listaDeCodigos.contains(codigo.codigo)) {
                 return "{\"ACK\": \n \"1\"}";
+
             } else {
                 return "{\"ACK\": \n \"0\"}";
             }
@@ -36,6 +46,10 @@ public class Main {
 
             return "Codigo de acesso cadastrado com sucesso: " + codigo.codigo;
         });
+        frame.add(codigoLabel);
+        frame.add(codigoTextField);
+
+        frame.setVisible(true);
     }
 
     private static class CodigoDeAcesso {
